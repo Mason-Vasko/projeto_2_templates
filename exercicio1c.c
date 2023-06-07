ff0794e8fa9ad165930cca34ac9ed6a7d01adb04
Abin <emaildamason@gmail.com>
2023-06-07 06:00:51 -0300

    implementação de a, b e c

--- exercicio1c.c
+++ exercicio1c.c
@@ -7,52 +7,61 @@ clock_t _ini, _fim;
 
 // Definição do tipo booleano
 unsigned char typedef bool;
-#define TRUE  1
+#define TRUE 1
 #define FALSE 0
 
-int* ler_inteiros(const char * arquivo, const int n)
-{
-    FILE* f = fopen(arquivo, "r");
+int *ler_inteiros(const char *arquivo, const int n) {
+  FILE *f = fopen(arquivo, "r");
 
-    int * inteiros = (int *) malloc(sizeof(int) * n);
+  int *inteiros = (int *)malloc(sizeof(int) * n);
 
-    for (int i = 0; !feof(f); i++)
-        fscanf(f, "%d\n", &inteiros[i]);
+  for (int i = 0; !feof(f); i++)
+    fscanf(f, "%d\n", &inteiros[i]);
 
-    fclose(f);
+  fclose(f);
 
-    return inteiros;
+  return inteiros;
 }
 
-void inicia_tempo()
-{
-    srand(time(NULL));
-    _ini = clock();
+void inicia_tempo() {
+  srand(time(NULL));
+  _ini = clock();
 }
 
-double finaliza_tempo()
-{
-    _fim = clock();
-    return ((double) (_fim - _ini)) / CLOCKS_PER_SEC;
+double finaliza_tempo() {
+  _fim = clock();
+  return ((double)(_fim - _ini)) / CLOCKS_PER_SEC;
+}
+void inverte(int *a, int *b) {
+  int aux = *a;
+  *a = *b;
+  *b = aux;
 }
 
-int main(int argc, char const *argv[])
-{
-    const int N = 50000;
-    unsigned encontrados = 0;
+int main(int argc, char const *argv[]) {
+  const int N = 50000;
+  unsigned encontrados = 0;
 
-    int* entradas = ler_inteiros("inteiros_entrada.txt", N);
-    int* consultas = ler_inteiros("inteiros_busca.txt", N);
+  int *entradas = ler_inteiros("inteiros_entrada.txt", N);
+  int *consultas = ler_inteiros("inteiros_busca.txt", N);
 
-    // realiza busca sequencia com realocação
-    inicia_tempo();
-    for (int i = 0; i < N; i++) {
-        // buscar o elemento consultas[i] na entrada
+  // realiza busca sequencia com realocação
+  inicia_tempo();
+  for (int i = 0; i < N; i++) {
+    // buscar o elemento consultas[i] na entrada
+    for (int j = 0; j < N; j++) {
+      if (consultas[i] == entradas[j]) {
+        encontrados++;
+        if (j != 0) {
+          inverte(&entradas[j], &entradas[j - 1]);
+        }
+      }
     }
-    double tempo_busca = finaliza_tempo();
+  }
+  double tempo_busca = finaliza_tempo();
 
-    printf("Tempo de busca    :\t%fs\n", tempo_busca);
-    printf("Itens encontrados :\t%d\n", encontrados);
+  printf("Tempo de busca    :\t%fs\n", tempo_busca);
+  printf("Itens encontrados :\t%d\n", encontrados);
 
-    return 0;
+  return 0;
 }
